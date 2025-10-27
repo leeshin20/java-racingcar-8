51d5f6358ca7820c5ca0fe076946dfdf06304384
leeshin20 <leeshin20@naver.com>
2025-10-27 21:12:07 +0900

    feat: 기본 출력 메시지 작성
    (cherry picked from commit 579b56adfbb6161d0dbdb2a0ed0742d789aacf1b)

---
+++ src/main/java/racingcar/Output.java
@@ -0,0 +1,15 @@
+package racingcar;
+
+public class Output {
+    public static void promptCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+
+    public static void promptAttemptCount() {
+        System.out.println("시도할 회수는 몇회인가요?");
+    }
+
+    public static void displayResult() {
+        System.out.println("\n실행 결과");
+    }
+}
