1fd356dd40bb0e43543e8fadc3b6ddf6370a7480
leeshin20 <leeshin20@naver.com>
2025-10-27 20:32:57 +0900

    test: 자동차 이름 분할 기능 테스트 코드 작성
    (cherry picked from commit fc2d525340001b91472247f1386cf9910e96407e)

---
+++ src/test/java/racingcar/InputTest.java
@@ -0,0 +1,16 @@
+package racingcar;
+
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class InputTest {
+    @Test
+    void 자동차_이름_분할_테스트() {
+        String names = "pobi,jun";
+        ArrayList<String> result = input.splitName(names);
+        assertThat(result).contains("pobi","jun");
+        assertThat(result).containsExactly("pobi","jun");
+    }
+}
