a6e1dd930b26b1aba18bf831b58738dcb9cd42f3
leeshin20 <leeshin20@naver.com>
2025-10-27 20:48:07 +0900

    test: 자동차 이름 입력 시 예외처리 기능 테스트 코드 작성

--- src/test/java/racingcar/InputTest.java
+++ src/test/java/racingcar/InputTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class InputTest {
     @Test
@@ -14,5 +15,11 @@ public class InputTest {
         assertThat(result).containsExactly("pobi","jun");
     }
 
-
+    @Test
+    void 자동차_이름_예외_처리_테스트() {
+        String input = "abc123,abc,aabb";
+        assertThatThrownBy(() -> Input.splitName(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("자동차 이름은 5글자까지 입력 가능합니다.");
+    }
 }
